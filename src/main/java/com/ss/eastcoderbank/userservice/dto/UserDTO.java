ffa14f6b613de7ce2d2548be7ebee3bb3e2d2117
Amanda R <amanda.rolon@smoothstack.com>
2021-07-06 19:43:26 -0400

    updates UserDTO info

---
+++ src/main/java/com/ss/eastcoderbank/userservice/dto/UserDTO.java
@@ -0,0 +1,35 @@
+package com.ss.eastcoderbank.userservice.dto;
+
+import com.ss.eastcoderbank.userservice.model.Address;
+import com.ss.eastcoderbank.userservice.model.Credential;
+import com.ss.eastcoderbank.userservice.model.UserRole;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class UserDTO {
+    private Integer id;
+
+    private UserRole role;
+
+    private String firstname;
+
+    private String lastname;
+
+    private LocalDate dob;
+
+    private String email;
+
+    private String phone;
+
+    private Address address;
+
+    private LocalDate dateJoined;
+
+    private boolean activeStatus;
+
+    private Credential credential;
+}
