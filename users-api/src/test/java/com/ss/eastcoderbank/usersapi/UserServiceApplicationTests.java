e1b8aaf4611ed4e762ee13e5781ee0f5a6b13e74
Amanda R <amanda@Amandas-MacBook-Pro.local>
2021-11-09 11:51:54 -0500

    adds assertions to test

--- users-api/src/test/java/com/ss/eastcoderbank/usersapi/UserServiceApplicationTests.java
+++ users-api/src/test/java/com/ss/eastcoderbank/usersapi/UserServiceApplicationTests.java
@@ -1,13 +1,41 @@
 package com.ss.eastcoderbank.usersapi;
 
+import com.ss.eastcoderbank.usersapi.controller.AuthorizationController;
+import com.ss.eastcoderbank.usersapi.controller.ExceptionController;
+import com.ss.eastcoderbank.usersapi.controller.UserController;
+import com.ss.eastcoderbank.usersapi.service.AuthService;
+import com.ss.eastcoderbank.usersapi.service.UserService;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class UserServiceApplicationTests {
 
+    @Autowired
+    private UserController userController;
+
+    @Autowired
+    private AuthorizationController authorizationController;
+
+    @Autowired
+    private ExceptionController exceptionController;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AuthService authService;
+
     @Test
-    public void contextLoads() {
+    void contextLoads() {
+        assertThat(userController).isNotNull();
+        assertThat(authorizationController).isNotNull();
+        assertThat(exceptionController).isNotNull();
+        assertThat(authService).isNotNull();
+        assertThat(userService).isNotNull();
     }
 
 }
